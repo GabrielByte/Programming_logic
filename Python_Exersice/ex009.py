333ff4178ac4c8ad50b26c067bdbd027886fa30c
GabrielByte <70727887+GabrielByte@users.noreply.github.com>
2021-01-17 15:45:41 -0300

    Update ex009.py

--- Python_Exersice/ex009.py
+++ Python_Exersice/ex009.py
@@ -1,7 +1,11 @@
+'''
+    Make a program that takes a number and shows its mutiplication table
+'''
+
 n1 = int(input("Enter a number: "))
 n2 = 1
 
 while n2 < 10:
     n3 = n1 * n2
-    print(n3)
+    print(f"{n1} x {n2} = {n3}")
     n2 += 1
