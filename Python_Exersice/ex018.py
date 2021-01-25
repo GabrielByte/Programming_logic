3a86ce3066400369a972e2a3a1cc7106a7d08f9a
GabrielByte <70727887+GabrielByte@users.noreply.github.com>
2021-01-25 18:28:19 -0300

    full commit

--- Python_Exersice/ex018.py
+++ Python_Exersice/ex018.py
@@ -1,8 +1,10 @@
 '''
     Make a program that calculates sine, cosine and tangent
 '''
-from math import sin, cos, tan
+from math import sin, cos, tan, radians
 
-angle = float(input("Enter a number: "))
+angle = radians(float(input("Enter an angle: ")))
 
-print(f"This is the result; sine: {sin(angle):.2f}; cosine: {cos(angle):.2f} and tangent: {tan(angle):.2f}")
+print(f"This is the result; Sine: {sin(angle):.2f}")
+print(f"Cosine: {cos(angle):.2f}") 
+print(f"and Tangent: {tan(angle):.2f}")
--- Python_Exersice/ex021.py
+++ Python_Exersice/ex021.py
@@ -1,7 +1,8 @@
 '''
     Make a program that opens an audio.mp3
 '''
-
-import os
-
-os.system("file's name.mp3")
+import pygame
+pygame.init()
+pygame.mixer.music.load("type the song here.mp3")
+pygame.mixer.music.play()
+pygame.event.wait()
